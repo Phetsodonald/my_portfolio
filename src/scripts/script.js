91558d13e2151391387a473b3357a5a236557b02
unknown <donaldphetso69@gmail.com>
2025-04-16 06:27:54 +0200

    added about,skills,projects and contact

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,6 +1,14 @@
 const toggle = document.querySelector(".menu-icon");
 const sidebar = document.querySelector(".sidebar");
+const contactForm = document.querySelector("#contact");
 
 toggle.addEventListener("click", () => {
   sidebar.style.display = sidebar.style.display === "flex" ? "none" : "flex";
+});
+
+
+contactForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  const inputs = contactForm.querySelectorAll("input, textarea");
+  inputs.forEach(input => input.value = "");
 });
